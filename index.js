fbbd1257aed97863f8688d1fdc840a7d4538e0b7
simple <evil.leon.8@gmail.com>
2023-10-10 18:24:53 -0400

    8080

--- index.js
+++ index.js
@@ -77,9 +77,9 @@ server.on("upgrade", (req, socket, head) => {
 });
 
 server.on("listening", () => {
-  console.log(`Interstellar running at http://localhost:${process.env.PORT}`);
+  console.log(`Interstellar running at http://localhost:8080`);
 });
 
 server.listen({
-  port: process.env.PORT,
+  port: 8080,
 });
